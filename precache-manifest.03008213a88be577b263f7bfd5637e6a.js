bec5ce152f85844b85c61cd18e221b758afa1dca
agryash <yash.agrawal2@learner.manipal.edu>
2021-02-07 13:44:04 +0530

    Updates

---
+++ precache-manifest.03008213a88be577b263f7bfd5637e6a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f067c7f1c6077d11dc272612734cf38",
+    "url": "/CatchOfTheDay/index.html"
+  },
+  {
+    "revision": "687d5027abe23a83bbce",
+    "url": "/CatchOfTheDay/static/css/main.3e97fb3f.chunk.css"
+  },
+  {
+    "revision": "e2753844bccfbd0283a2",
+    "url": "/CatchOfTheDay/static/js/2.9d77aab9.chunk.js"
+  },
+  {
+    "revision": "9b50f5741be789e4838182ccc7c0c6a5",
+    "url": "/CatchOfTheDay/static/js/2.9d77aab9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "687d5027abe23a83bbce",
+    "url": "/CatchOfTheDay/static/js/main.ae5ad109.chunk.js"
+  },
+  {
+    "revision": "1674f2af0b3a4c0b0715",
+    "url": "/CatchOfTheDay/static/js/runtime-main.354b5b23.js"
+  },
+  {
+    "revision": "d2f8799a13e6db2022d9a7de22776705",
+    "url": "/CatchOfTheDay/static/media/anchor.d2f8799a.svg"
+  },
+  {
+    "revision": "1461f8e9d2084c70c00830348efe1766",
+    "url": "/CatchOfTheDay/static/media/blanch_caps_inline-webfont.1461f8e9.woff"
+  },
+  {
+    "revision": "73a576a8254aeafebcdc66fe9fd41ac8",
+    "url": "/CatchOfTheDay/static/media/blanch_caps_inline-webfont.73a576a8.eot"
+  },
+  {
+    "revision": "7bbffda6bcacac8360f0ee00bb03dc04",
+    "url": "/CatchOfTheDay/static/media/blanch_caps_inline-webfont.7bbffda6.svg"
+  },
+  {
+    "revision": "8345e1b3362c1d640f0f11b5aae1d5bc",
+    "url": "/CatchOfTheDay/static/media/blanch_caps_inline-webfont.8345e1b3.ttf"
+  },
+  {
+    "revision": "4c3fb584d3f63821357c8a9cff97f360",
+    "url": "/CatchOfTheDay/static/media/haymaker-webfont.4c3fb584.eot"
+  },
+  {
+    "revision": "751e56274d3ea1c651ea824f902bb987",
+    "url": "/CatchOfTheDay/static/media/haymaker-webfont.751e5627.woff"
+  },
+  {
+    "revision": "cf798e30eda73d84b25c4ae233fe5e21",
+    "url": "/CatchOfTheDay/static/media/haymaker-webfont.cf798e30.svg"
+  },
+  {
+    "revision": "e9535f20ca175cc3fdfe2816f7c47168",
+    "url": "/CatchOfTheDay/static/media/haymaker-webfont.e9535f20.ttf"
+  }
+]);
