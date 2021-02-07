dc6fc26389653bcb70d23bb73f9e52097d84391a
agryash <yash.agrawal2@learner.manipal.edu>
2021-02-07 15:08:46 +0530

    Fixed Images

--- src/sample-fishes.js
+++ src/sample-fishes.js
@@ -1,8 +1,7 @@
-// This is just some sample data so you don't have to think of your own!
 const fishes = {
   fish1: {
     name: "Pacific Halibut",
-    image: "/images/hali.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/catchoftheday-1b134.appspot.com/o/hali.jpg?alt=media&token=8f7c4319-506e-4b04-91af-80a924250510",
     desc:
       "Everyone’s favorite white fish. We will cut it to the size you need and ship it.",
     price: 1724,
@@ -11,7 +10,7 @@ const fishes = {
 
   fish2: {
     name: "Lobster",
-    image: "/images/lobster.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/catchoftheday-1b134.appspot.com/o/lobster.jpg?alt=media&token=0fce27f7-0ad0-408e-a8f6-f3f3ca25e183",
     desc:
       "These tender, mouth-watering beauties are a fantastic hit at any dinner party.",
     price: 3200,
@@ -20,7 +19,7 @@ const fishes = {
 
   fish3: {
     name: "Sea Scallops",
-    image: "/images/scallops.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/catchoftheday-1b134.appspot.com/o/scallops.jpg?alt=media&token=622364e6-20ed-4b63-9733-95589f46b227",
     desc:
       "Big, sweet and tender. True dry-pack scallops from the icey waters of Alaska. About 8-10 per pound",
     price: 1684,
@@ -29,7 +28,7 @@ const fishes = {
 
   fish4: {
     name: "Mahi Mahi",
-    image: "/images/mahi.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/catchoftheday-1b134.appspot.com/o/mahi.jpg?alt=media&token=6b42ac38-b7f2-43bf-b50e-66f3a6d98b90",
     desc:
       "Lean flesh with a mild, sweet flavor profile, moderately firm texture and large, moist flakes. ",
     price: 1129,
@@ -38,7 +37,7 @@ const fishes = {
 
   fish5: {
     name: "King Crab",
-    image: "/images/crab.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/catchoftheday-1b134.appspot.com/o/crab.jpg?alt=media&token=3696e476-86aa-405d-901b-2471082b74fa",
     desc:
       "Crack these open and enjoy them plain or with one of our cocktail sauces",
     price: 4234,
@@ -47,7 +46,7 @@ const fishes = {
 
   fish6: {
     name: "Atlantic Salmon",
-    image: "/images/salmon.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/catchoftheday-1b134.appspot.com/o/salmon.jpg?alt=media&token=f71625e8-226c-47cf-a7ed-2251e47893a5",
     desc:
       "This flaky, oily salmon is truly the king of the sea. Bake it, grill it, broil it...as good as it gets!",
     price: 1453,
@@ -56,7 +55,7 @@ const fishes = {
 
   fish7: {
     name: "Oysters",
-    image: "/images/oysters.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/catchoftheday-1b134.appspot.com/o/oysters.jpg?alt=media&token=b046eaad-f2d1-4ef9-ab24-c1b4f2996a52",
     desc: "A soft plump oyster with a sweet salty flavor and a clean finish.",
     price: 2543,
     status: "available"
@@ -64,7 +63,7 @@ const fishes = {
 
   fish8: {
     name: "Mussels",
-    image: "/images/mussels.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/catchoftheday-1b134.appspot.com/o/mussels.jpg?alt=media&token=8a7950d5-ef33-4af1-86c6-c68d372adf76",
     desc:
       "The best mussels from the Pacific Northwest with a full-flavored and complex taste.",
     price: 425,
@@ -73,7 +72,7 @@ const fishes = {
 
   fish9: {
     name: "Jumbo Prawns",
-    image: "/images/prawns.jpg",
+    image: "https://firebasestorage.googleapis.com/v0/b/catchoftheday-1b134.appspot.com/o/prawns.jpg?alt=media&token=64184ea2-b153-4247-8cef-18f218fd7449",
     desc:
       "With 21-25 two bite prawns in each pound, these sweet morsels are perfect for shish-kabobs.",
     price: 2250,
